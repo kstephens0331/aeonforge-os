508a1a70fc821d097ee8fadfec1066b87b397bff
kstephens0331 <Kyle_stephens31@icloud.com>
2025-07-12 13:10:14 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-import BuilderDashboard from './pages/BuilderDashboard';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import BuilderDashboard from './pages/BuilderDashboard.jsx';
 
-<Route path="/builder" element={<BuilderDashboard />} />
+function App() {
+  return (
+    <Routes>
+      <Route path="/builder" element={<BuilderDashboard />} />
+      <Route path="/" element={<h1>Welcome to Aeonforge OS</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
